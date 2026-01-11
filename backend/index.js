e362caf08a71d5f4a5e921bd8c11fa3a0962f33f
Md. Faiz Ansari <mrfaizansari111@gmail.com>
2026-01-11 19:04:50 +0530

    Update CORS origin to remove port number

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://13.201.172.166:80',
+    origin:'http://13.201.172.166',
     credentials:true
 }
 
