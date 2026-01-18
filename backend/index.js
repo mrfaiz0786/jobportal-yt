b4ebc5117c4f6450cf2c20f06d9436825603e112
Md. Faiz Ansari <mrfaizansari111@gmail.com>
2026-01-18 11:41:09 +0530

    Update CORS origin to new IP address

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://13.201.172.166',
+    origin:'http://16.171.175.78',
     credentials:true
 }
 
