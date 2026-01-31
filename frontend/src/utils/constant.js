e68d4fa9d0726f3e8381edfd3db31c2be40de5a7
Md. Faiz Ansari <mrfaizansari111@gmail.com>
2026-01-31 22:30:59 +0530

    Update API endpoints to new server address

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,4 @@
-export const USER_API_END_POINT="http://16.171.175.78:8000/api/v1/user";
-export const JOB_API_END_POINT="http://16.171.175.78:8000/api/v1/job";
-export const APPLICATION_API_END_POINT="http://16.171.175.78:8000/api/v1/application";
-export const COMPANY_API_END_POINT="http://16.171.175.78:8000/api/v1/company";
+export const USER_API_END_POINT="http://16.171.147.224:8000/api/v1/user";
+export const JOB_API_END_POINT="http://16.171.147.224:8000/api/v1/job";
+export const APPLICATION_API_END_POINT="http://16.171.147.224:8000/api/v1/application";
+export const COMPANY_API_END_POINT="http://16.171.147.224:8000/api/v1/company";
